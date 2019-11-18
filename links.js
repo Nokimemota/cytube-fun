bf81755ff6758d85bb12ae52ccfb12cbc7e75ddc
Nokimemota <otaku0kun@gmail.com>
2019-11-18 16:50:18 +0100

    Create links.js
    created file that contains wallpaper links

---
+++ links.js
@@ -0,0 +1,6 @@
+var links = ['https://images3.alphacoders.com/129/129197.jpg',
+'https://images.alphacoders.com/530/530304.jpg',
+'https://images4.alphacoders.com/129/129196.jpg',
+'https://images8.alphacoders.com/919/919956.jpg',
+'https://download.hipwallpaper.com/desktop/1440/900/85/95/TYsGur.jpg',
+'https://cdn.hipwallpaper.com/i/42/16/IvgYBa.jpg']
