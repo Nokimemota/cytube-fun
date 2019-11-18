1cbed697401f1c4366730c22e393f29418d1dd8c
Nokimemota <otaku0kun@gmail.com>
2019-11-18 17:40:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -701,7 +701,7 @@ var links = ['https://images3.alphacoders.com/129/129197.jpg',
 'https://images4.alphacoders.com/129/129196.jpg',
 'https://images8.alphacoders.com/919/919956.jpg',
 'https://download.hipwallpaper.com/desktop/1440/900/85/95/TYsGur.jpg',
-'https://cdn.hipwallpaper.com/i/42/16/IvgYBa.jpg']
+'https://cdn.hipwallpaper.com/i/42/16/IvgYBa.jpg'];
 
 selectBG = bgColorArray[Math.floor(Math.random() * bgColorArray.length)];
 $('#backg').css('background', 'url(' + selectBG + ')')
