c45826285bace74c6fe252567b9a71120a4c4253
Nokimemota <otaku0kun@gmail.com>
2019-11-18 17:41:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -696,12 +696,7 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-53755606-1', 'auto');
 ga('send', 'pageview');
 
-var links = ['https://images3.alphacoders.com/129/129197.jpg',
-'https://images.alphacoders.com/530/530304.jpg',
-'https://images4.alphacoders.com/129/129196.jpg',
-'https://images8.alphacoders.com/919/919956.jpg',
-'https://download.hipwallpaper.com/desktop/1440/900/85/95/TYsGur.jpg',
-'https://cdn.hipwallpaper.com/i/42/16/IvgYBa.jpg'];
+var links = ['https://images8.alphacoders.com/919/919956.jpg']
 
 selectBG = bgColorArray[Math.floor(Math.random() * bgColorArray.length)];
 $('#backg').css('background', 'url(' + selectBG + ')')
