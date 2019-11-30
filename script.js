a3de2ccd79175315d307b42bc6d3ef08fc5096ca
Nokimemota <otaku0kun@gmail.com>
2019-11-30 01:57:34 +0100

    wallpper change

--- script.js
+++ script.js
@@ -696,12 +696,13 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-53755606-1', 'auto');
 ga('send', 'pageview');
 	
-var bgColorArray = ['https://images3.alphacoders.com/129/129197.jpg',
-'https://images.alphacoders.com/530/530304.jpg',
-'https://images4.alphacoders.com/129/129196.jpg',
-'https://images8.alphacoders.com/919/919956.jpg',
-'https://download.hipwallpaper.com/desktop/1440/900/85/95/TYsGur.jpg',
-'https://cdn.hipwallpaper.com/i/42/16/IvgYBa.jpg']
+var bgColorArray = ['https://imgur.com/oH3ESfw',
+'https://imgur.com/NAxBPSs',
+'https://imgur.com/qLHSPek',
+'https://imgur.com/aQtrwbx',
+'https://imgur.com/sjmfHgf',
+'https://imgur.com/iY9Rk0o',
+'https://imgur.com/3gSmHLl']
 
 selectBG = bgColorArray[Math.floor(Math.random() * bgColorArray.length)];
 $('#backg').css('background', 'url(' + selectBG + ')')
